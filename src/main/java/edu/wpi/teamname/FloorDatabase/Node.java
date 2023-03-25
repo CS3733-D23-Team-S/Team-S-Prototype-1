d4c1e348eae545c4185062269682b3a1294bdb49
TBrown2024 <tbrown@wpi.edu>
2023-03-25 16:02:42 -0400

    Created Getters and Setters for Values in Node and Created Enums for Floors and NodeType

--- src/main/java/edu/wpi/teamname/FloorDatabase/Node.java
+++ src/main/java/edu/wpi/teamname/FloorDatabase/Node.java
@@ -1,6 +1,22 @@
 package edu.wpi.teamname.FloorDatabase;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Node
 {
-
+    @Getter @Setter
+    private int xCord;
+    @Getter @Setter
+    private int yCord;
+    @Getter @Setter
+    private Floor floor;
+    @Getter @Setter
+    private String building;
+    @Getter @Setter
+    private NodeType nodeType;
+    @Getter @Setter
+    private String longName;
+    @Getter @Setter
+    private String shortName;
 }
---
+++ src/main/java/edu/wpi/teamname/FloorDatabase/NodeType.java
@@ -0,0 +1,6 @@
+package edu.wpi.teamname.FloorDatabase;
+
+public enum NodeType
+{
+    CONF, DEPT, ELEV, EXIT, HALL, LABS, REST, RETL, SERV, STAI
+}
