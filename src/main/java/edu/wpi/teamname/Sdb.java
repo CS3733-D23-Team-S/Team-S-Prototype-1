f82939c3fbfd1028c27872d87d4da2613c3e922b
Nikesh <nikeshwalling@gmail.com>
2023-03-28 12:37:50 -0400

    Getting the table creation to work in the backend

--- src/main/java/edu/wpi/teamname/Sdb.java
+++ src/main/java/edu/wpi/teamname/Sdb.java
@@ -1,7 +1,6 @@
 package edu.wpi.teamname;
 
 import edu.wpi.teamname.FloorDatabase.Edge;
-import edu.wpi.teamname.FloorDatabase.Node;
 import edu.wpi.teamname.FloorDatabase.NodesDAOImpl;
 import edu.wpi.teamname.FloorDatabase.csvConverter;
 import java.sql.SQLException;
@@ -11,6 +10,7 @@ public class Sdb {
     csvConverter converter = new csvConverter();
     converter.csvToNode("src/main/java/edu/wpi/teamname/L1Nodes.csv");
     converter.csvToEdges("src/main/java/edu/wpi/teamname/L1Edges.csv");
+
     for (Edge thisEdge : converter.getEdges()) {
       System.out.println(thisEdge.toString());
     }
@@ -18,8 +18,8 @@ public class Sdb {
     NodesDAOImpl mapDatabase = new NodesDAOImpl();
     mapDatabase.establishConnection();
     mapDatabase.initTable();
-    for (Node thisNode : converter.getNodes().values()) {
-      mapDatabase.addNode(thisNode);
-    }
+//        for (Node thisNode : converter.getNodes().values()) {
+//          mapDatabase.addNode(thisNode);
+//        }
   }
 }
