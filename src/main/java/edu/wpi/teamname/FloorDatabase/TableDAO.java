d1563265bb86fd2eb8a91db93115c23b2f131855
TBrown2024 <tbrown@wpi.edu>
2023-03-27 20:04:53 -0400

    TableDAO and TableDAOI Updated. Connection moved to Sdb

--- src/main/java/edu/wpi/teamname/FloorDatabase/TableDAO.java
+++ src/main/java/edu/wpi/teamname/FloorDatabase/TableDAO.java
@@ -1,4 +1,32 @@
 package edu.wpi.teamname.FloorDatabase;
 
-public class TableDAO {
+import java.sql.SQLException;
+import java.util.List;
+
+public class TableDAO implements TableDAOI
+{
+        public List<Object> getAllRows()
+        {
+            return null;
+        }
+
+        public void updateRow(Object target)
+        {
+
+        }
+
+        public void deleteRow(Object target)
+        {
+
+        }
+
+        public void initTable() throws SQLException
+        {
+
+        }
+
+        public void resetData() throws SQLException
+        {
+
+        }
 }
--- src/main/java/edu/wpi/teamname/FloorDatabase/TableDAOI.java
+++ src/main/java/edu/wpi/teamname/FloorDatabase/TableDAOI.java
@@ -1,4 +1,17 @@
 package edu.wpi.teamname.FloorDatabase;
 
-public interface TableDAOI {
+import java.sql.SQLException;
+import java.util.List;
+
+public interface TableDAOI
+{
+    List<Object> getAllRows();
+
+    void updateRow(Object target);
+
+    void deleteRow(Object target);
+
+    void initTable() throws SQLException;
+
+    void resetData() throws SQLException;
 }
