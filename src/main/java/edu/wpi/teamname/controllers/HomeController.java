fd3961f826a91d30c11a035a88d534f4f47e095e
Prahladh Raja <pnamboorkrishnam@wpi.edu>
2023-03-28 18:58:32 -0400

    added homepage and menu option changes

--- src/main/java/edu/wpi/teamname/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamname/controllers/HomeController.java
@@ -4,25 +4,30 @@ import edu.wpi.teamname.navigation.Navigation;
 import edu.wpi.teamname.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import java.awt.*;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 
 public class HomeController {
-  private VBox buttonLayout;
+
   @FXML MFXButton mealDeliveryButton;
   @FXML MFXButton reserveRoomButton;
 
   @FXML MFXButton signageButton;
 
+  @FXML MenuButton menuButton;
+  @FXML MenuItem exitOption;
+
   @FXML
   public void initialize() {
     // Adding the menu option to exit application
-    Menu menu = new Menu("Menu");
-    MenuItem option1 = new MenuItem("Exit");
-    menu.add(option1);
+
     mealDeliveryButton.setOnMouseClicked(event -> goToMealPage());
     reserveRoomButton.setOnMouseClicked(event -> goToRoomPage());
     signageButton.setOnMouseClicked(event -> goToSignagePage());
+    exitOption.setOnAction(event -> exitApplication());
 
     // event handler for exiting application
 
@@ -40,6 +45,10 @@ public class HomeController {
   }
 
   public void goToSignagePage() {
-    Navigation.navigate(Screen.SERVICE_REQUEST);
+    Navigation.navigate(Screen.SIGNAGE_PAGE);
+  }
+
+  public void exitApplication() {
+    Platform.exit();
   }
 }
