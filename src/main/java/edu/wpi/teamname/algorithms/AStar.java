72a267958320f0ccc2ee264e81742dbab78bd61b
Fatducky1 <ukulelerubin@gmail.com>
2023-03-27 20:11:41 -0400

    Added AStar

--- src/main/java/edu/wpi/teamname/algorithms/AStar.java
+++ src/main/java/edu/wpi/teamname/algorithms/AStar.java
@@ -1,24 +1,51 @@
 package edu.wpi.teamname.algorithms;
 
 import edu.wpi.teamname.FloorDatabase.Node;
+import edu.wpi.teamname.controllers.DataBaseController;
 
 import java.util.*;
 
 public class AStar {
+	DataBaseController dataBase = new DataBaseController();
 
 	public AStar(){};
 
 	public List<Node> findPath(Node start, Node end){
-		final Queue<Node> nodesYetToSearch = new LinkedList<Node>();
-		final Map<Node, Boolean> visitedNodes = new HashMap<Node, Boolean>();
-		final Map<Node, Node> gotHereFrom = new HashMap<Node, Node>();
+		HeuristicNode startHNode = new HeuristicNode(start, calculateWeight(start, end));
+		final PriorityQueue<HeuristicNode> nodesYetToSearch = new PriorityQueue<>();
+		final Map<HeuristicNode, Boolean> visitedNodes = new HashMap<>();
+		final Map<Node, HeuristicNode> gotHereFrom = new HashMap<>();
+
+		nodesYetToSearch.add(startHNode);
+		//Node currentNode = start;
+		HeuristicNode currentNode;
+
+		while (nodesYetToSearch.size() != 0) {
+			currentNode = nodesYetToSearch.poll();
+
+			for (Node nodeToSearch : dataBase.getNeighbors(currentNode.node)) {
+
+
+				if (!visitedNodes.containsKey(nodeToSearch)) {
+					int weight = calculateWeight(nodeToSearch, end);
+
+					nodesYetToSearch.add(new HeuristicNode(nodeToSearch, weight));
+					gotHereFrom.put(nodeToSearch, currentNode);
+				}
+
+
+			}
+			visitedNodes.put(currentNode, true);
+
+		}
 
 	return null;
 	}
 
 
-	private int calcHeuristicVal(Node start, Node target){
-		return 0;
+	private int calculateWeight(Node start, Node target){
+		int weight = Math.sqrt(Math.pow((start.getXCoord() - target.getXCoord),2), Math.pow((start.getYCoord - target.getYCoord),1));
+		return weight;
 	}
 
 
@@ -35,5 +62,21 @@ public class AStar {
 
 	}
 
+	class HeuristicNode implements Comparator<HeuristicNode> {
+		Node node;
+		int weight;
+
+		public HeuristicNode(Node node, int weight) {
+			this.node = node;
+			this.weight = weight;
+		}
+
+		@Override
+		public int compare(HeuristicNode o1, HeuristicNode o2) {
+			return Integer.compare(o1.weight, o2.weight);
+		}
+	}
 
 }
+
+
