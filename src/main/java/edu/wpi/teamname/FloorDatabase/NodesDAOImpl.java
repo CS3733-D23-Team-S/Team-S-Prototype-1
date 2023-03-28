5e6ff54be620f82ac8e82d4632a5e778409d43c8
Anthony <atitcombe13@gmail.com>
2023-03-28 18:15:39 -0400

    corrected the add node parsing

--- src/main/java/edu/wpi/teamname/FloorDatabase/NodesDAOImpl.java
+++ src/main/java/edu/wpi/teamname/FloorDatabase/NodesDAOImpl.java
@@ -47,13 +47,13 @@ public class NodesDAOImpl extends DAOImpl implements DAO_I {
                   + floorNodeTableName
                   + " (nodeID ,xCoord ,yCoord , Floor ,Building, longName, shortName) "
                   + " VALUES (?, ?, ? ,?, ?, ?, ?)");
-      preparedStatement.setString(1, "'" + thisNode.getNodeID() + "'");
+      preparedStatement.setString(1, thisNode.getNodeID());
       preparedStatement.setInt(2, thisNode.getXCoord());
       preparedStatement.setInt(3, thisNode.getYCoord());
       preparedStatement.setInt(4, thisNode.getFloor().ordinal());
-      preparedStatement.setString(5, "'" + thisNode.getBuilding() + "'");
-      preparedStatement.setString(6, "'" + thisNode.getLongName() + "'");
-      preparedStatement.setString(7, "'" + thisNode.getShortName() + "'");
+      preparedStatement.setString(5,  thisNode.getBuilding());
+      preparedStatement.setString(6, thisNode.getLongName());
+      preparedStatement.setString(7, thisNode.getShortName());
 
       preparedStatement.executeUpdate();
     } catch (SQLException e) {
