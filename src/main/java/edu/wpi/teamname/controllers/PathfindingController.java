8136314a1692cf02cf4af135ecdd78e1809ce15c
wrigh <rcwright@wpi.edu>
2023-03-29 21:42:26 -0400

    home.fxml file

--- src/main/java/edu/wpi/teamname/controllers/PathfindingController.java
+++ src/main/java/edu/wpi/teamname/controllers/PathfindingController.java
@@ -1,15 +1,24 @@
 package edu.wpi.teamname.controllers;
 
+import edu.wpi.teamname.PathfindingEntity;
 import edu.wpi.teamname.navigation.Navigation;
 import edu.wpi.teamname.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 
 public class PathfindingController {
   @FXML MFXButton pathfindingToHomeButton;
 
-  @FXML
+  @FXML MFXButton findPathButton;
+
+  @FXML MFXTextField startingLocation;
+
+  @FXML MFXTextField destination;
+
   public void initialize() {
     pathfindingToHomeButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    findPathButton.setOnMouseClicked(
+        event -> new PathfindingEntity(startingLocation.getText(), destination.getText()));
   }
 }
