e5ed1a6937fef40591375ce43abde440b9d96d86
wrigh <rcwright@wpi.edu>
2023-03-29 21:42:26 -0400

    pathfinding ORM

---
+++ src/main/java/edu/wpi/teamname/PathfindingEntity.java
@@ -0,0 +1,13 @@
+package edu.wpi.teamname;
+
+public class PathfindingEntity {
+
+  String startingLocation;
+
+  String destination;
+
+  public PathfindingEntity(String startingLocation, String destination) {
+    this.startingLocation = startingLocation;
+    this.destination = destination;
+  }
+}
